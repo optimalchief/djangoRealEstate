0b3048aa605008af8f237ea61695d5acaa56091f
optimalchief <velazquez234@yahoo.com>
2022-04-04 17:10:41 -0400

    settings folder

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realEstate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realEstate.settings.developement')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
